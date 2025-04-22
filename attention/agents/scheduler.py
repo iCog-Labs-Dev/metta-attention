ad76145414e187897d0b2b97f2de3152758104b8
BlightG <birhane.gulilat@gmail.com>
2025-04-22 16:17:05 +0300

    modified output to af_agent.log output and added log to csv file

--- attention/agents/scheduler.py
+++ attention/agents/scheduler.py
@@ -3,6 +3,8 @@ import time
 import sys
 import os
 import logging
+import csv
+from datetime import datetime
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from agents.agent_base import AgentObject
@@ -17,6 +19,7 @@ class ParallelScheduler:
 
         # Configure logging
         self.log_file = log_file
+        self.csv_file = self.get_csv_file_name()
         logging.basicConfig(filename=self.log_file, level=logging.INFO,
                             format='%(asctime)s - %(levelname)s - %(message)s')
         logging.info("Starting ParallelScheduler")
@@ -41,6 +44,13 @@ class ParallelScheduler:
         
         return self.agent_instances[agent_id]
 
+    def get_csv_file_name(self) -> str:
+        """ creats a name and file for the current instance of the controller """
+
+        time = datetime.now().strftime("%H:%M:%S-%d-%m-%Y")
+        file_name = f"csv/results_{time}.csv"
+        return file_name
+
     def log_af_state(self, agent: AgentObject, agent_id: str):
         """Logs the attentionalFocus state of the AFImportanceDiffusionAgent."""
 #        try:
@@ -59,9 +69,10 @@ class ParallelScheduler:
                     }
         try:
             agent.run()
-            results = agent._metta.run(commands[agent_id])
-            af_state = results[0] if results else "No result"
-            logging.info(f"{agent_id} state {af_state}")
+            # results = agent._metta.run(commands[agent_id])
+            # af_state = results[0] if results else "No result"
+            logging.info(f"{agent_id} running")
+            print(f"{agent_id} running")
         except Exception as e:
             logging.error(f"Error logging attentionalFocus: {e}")
             print(f"Error logging attentionalFocus: {e}")
@@ -89,7 +100,35 @@ class ParallelScheduler:
 
                     # Wait for all agents to complete before starting the next iteration
                     concurrent.futures.wait(futures)
+                    
+                    timestamp = datetime.now().isoformat()
+                    log = []
+                    if len(self.agent_creators) > 0:
+                        # agent = self.get_or_create_agent("HebbianCreationAgent")
+                        result = self.metta.run("!(match (attentionalFocus) $x ($x (getSTI $x)))")
+                        if result[0]:
+                            for expatom in result[0]:
+                                print(f"result {expatom}")
+                                log.append(expatom.get_children())
+                        
+                    data = []
+                    if log:
+                        for l in log :
+                            data.append({"timestamp": timestamp,  "pattern": l[0], "sti": l[1]})
+                    # else:
+                    #     data = [{"timestamp": timestamp,  "pattern": "-", "sti": "0"}]
 
+                    with open(self.csv_file, 'a', newline='') as f:
+                        writer = csv.DictWriter(f, fieldnames=["timestamp", "pattern", "sti"])
+
+                        if f.tell() == 0:
+                            writer.writeheader()
+                        
+                        if len(data) > 1:
+                            for d in data:
+                                writer.writerow(d)
+                        # else:
+                        #     writer.writerow(data[0])
                     # Log attentionalFocus state for the AF agent
                     # af_agent = self.get_or_create_agent(self.af_agent_id) #get agent AF
                     # if af_agent:
@@ -104,4 +143,3 @@ class ParallelScheduler:
         except Exception as e:
             logging.error(f"Exception in run_continuously: {e}")
             print(f"Exception in run_continuously: {e}")
-    
