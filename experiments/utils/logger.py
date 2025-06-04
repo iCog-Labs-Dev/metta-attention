3e55864674ec8b3d52445f612b2cd87a92808635
BlightG <birhane.gulilat@gmail.com>
2025-06-04 09:40:29 +0300

    added save_params function to write Ecan parameters to json file

--- experiments/utils/logger.py
+++ experiments/utils/logger.py
@@ -2,6 +2,7 @@ from hyperon.ext import register_atoms
 from hyperon.atoms import OperationAtom, S
 from datetime import datetime
 import csv
+import json
 
 
 
@@ -32,6 +33,21 @@ def write_to_csv(afatoms, afsize, name):
 
     return [S("wrote")]
 
+def save_params(params):
+    """ writes the params into a json file """
+
+    data = {}
+    for param in params.get_children():
+        key, value = param.get_children()
+        key = str(key)
+        value = str(value)
+        data[key] = value
+
+    with open("output/settings.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+    return [S('()')]
+
 
 @register_atoms(pass_metta=True)
 def utils(metta):
@@ -47,6 +63,10 @@ def utils(metta):
         "write_to_csv",
         lambda afatoms, afsize, name: write_to_csv(afatoms, afsize, name),
         ["Expression", "Expression", "Expression","Atom"],
+    saveParams = OperationAtom(
+        "save_params",
+        lambda param: save_params(param),
+        ["Expression", "Atom"],
         unwrap=False
         )
-    return {r"get_csv_file_name": getCsvFileName, r"write_to_csv": writeToCsv}
+    return {r"get_csv_file_name": getCsvFileName, r"write_to_csv": writeToCsv, r"save_params": saveParams}
